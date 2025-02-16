d6cf73e6539381953df0cb1db751c100067210e0
habeel bhatti <habeelbhatti873@gmail.com>
2025-02-16 21:07:22 +0500

    add message

---
+++ src/Maincomponents/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      contact ok working kar raha hai
+    </div>
+  )
+}
+
+export default Contact
--- src/Maincomponents/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Skills = () => {
-  return (
-    <div>Skills bhi ok kam kar raha ha </div>
-  )
-}
-
-export default Skills
