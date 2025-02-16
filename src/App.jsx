21231c6808bb9b32a6349fa778b49f007c18ec7a
habeel bhatti <habeelbhatti873@gmail.com>
2025-02-16 21:19:36 +0500

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Maincomponents/Home'
+import About from './Maincomponents/About'
+import Contact from './Maincomponents/Contact'
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
