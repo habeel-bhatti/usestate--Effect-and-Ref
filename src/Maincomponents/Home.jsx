8bde44b9a6601fef515d2e04ef7068c8605d70c8
habeel bhatti <habeelbhatti873@gmail.com>
2025-02-24 13:46:44 +0500

    add file

--- src/Maincomponents/Home.jsx
+++ src/Maincomponents/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div
     >Home ok work kar raha ha 
     <p>KYA hal chal ha app bhi routing karna chata ha </p>
-    <img src="https://images.unsplash.com/photo-1739477021524-0266d3dabec9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D" alt="image"  />
+    <img src="./images/sale.jpg" alt="" />
     </div>
   )
 }
