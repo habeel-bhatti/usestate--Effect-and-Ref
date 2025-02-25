8d7b06e90c9470a08123921c397686962cde3202
habeel bhatti <habeelbhatti873@gmail.com>
2025-02-25 18:59:59 +0500

    add message

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
-import Home from './Maincomponents/Home'
-import About from './Maincomponents/About'
-import Contact from './Maincomponents/Contact'
-import './App.css'
-import Navbar from './Maincomponents/Navbar';
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <h1>welcome to my website</h1>
   );
 }
 
